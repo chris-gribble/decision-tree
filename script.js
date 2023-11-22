2b5a30afbd7c65b0cee08cfe4351702cb331eed9
Chris Gribble <chris.gribble@hotmail.com>
2023-11-22 22:56:01 +1000

    UI improvements and fixed back btn stack

--- script.js
+++ script.js
@@ -3,8 +3,15 @@
 var data = getData();
 var stack = [];
 
-function handleButtonClick(message) {
-    alert(message);
+function updatePersistantBtns(nodeId)
+{
+    const persistantBtns = document.getElementById('persistant_btns');
+    
+    if (nodeId == "1") {
+        persistantBtns.style.display = 'none';
+    } else {
+        persistantBtns.style.display = 'block';
+    }
 }
 
 function displayNode(nodeId) {
@@ -22,14 +29,12 @@ function displayNode(nodeId) {
     myDiv.innerHTML += createQuestionElements(node.question);
 
     Object.values(node.buttons).forEach(btn => {
-        console.log("Button: " + btn.button_text);
-        console.log("Button: " + btn.hover_text);
-
         myDiv.innerHTML += createOptionElements(btn.button_text, btn.button_link);
     }); 
 
+    $("#dt_div").fadeIn("300");
 
-    $("#dt_div").fadeIn("slow");
+    updatePersistantBtns(nodeId);
 }
 
 function createQuestionElements(question)
@@ -39,17 +44,17 @@ function createQuestionElements(question)
 
 function createOptionElements(optionText, nodeId)
 {
-    return '<button class="btn-zingtree answers" onClick="displayNode(\''+nodeId+'\')">' + optionText + '</button>';
+    return '<button class="btn-zingtree option-btn" onClick="displayNode(\''+nodeId+'\')">' + optionText + '</button>';
 }
 
 function createTitleElement(title)
 {
-    return "<h3 class='content'>" + title + "</h3>"
+    return "<h3>" + title + "</h3>"
 }
 
 function createContentsElement(content)
 {
-    return "<p class='content'>" + content + "</p>"
+    return '<div class="content">' + content + '</div>';
 }
 
 function getDtElement()
@@ -64,7 +69,7 @@ function goBack()
 {
     if (stack.length > 1) {
         stack.pop();
-        displayNode(stack[stack.length-1]);
+        displayNode(stack.pop());
     }
 }
 
